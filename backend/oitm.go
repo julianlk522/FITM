b7dc37dea45c3bab4e269cd9cf3eb64853d20472
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2024-04-30 00:20:03 -0400

    delete-summary / delete-summary-like routes and controller funcs

--- backend/oitm.go
+++ backend/oitm.go
@@ -15,12 +15,7 @@ import (
 )
 
 func main() {
-	/* API Actions
-
-	SUMMARIES:
-	-Add new summary
-	-Like summary
-	-Delete summary
+	/* Todo API Actions
 	
 	LINKS:
 	-Copy extisting link to user's treasure map
@@ -206,7 +201,7 @@ func main() {
 
 			// Check if summary exists, Abort if not
 			var s sql.NullString
-			err = db.QueryRow("SELECT id FROM Summaries WHERE id = ?", summary_data.SummaryID).Scan(&s)
+			err = db.QueryRow("SELECT id FROM Summaries WHERE id = ?", summary_data.SummaryLikeRequest.SummaryID).Scan(&s)
 			if err != nil {
 				render.Render(w, r, ErrInvalidRequest(errors.New("summary not found")))
 				return
@@ -222,6 +217,48 @@ func main() {
 
 		render.Status(r, http.StatusCreated)
 		render.JSON(w, r, summary_data)
+	})
+
+	// Delete Summary / Unlike Summary
+	r.Delete("/summaries", func(w http.ResponseWriter, r *http.Request) {
+		summary_data := &SummaryRequest{}
+
+		if err := render.Bind(r, summary_data); err != nil {
+			render.Render(w, r, ErrInvalidRequest(err))
+			return
+		}
+
+		db, err := sql.Open("sqlite3", "./db/oitm.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer db.Close()
+
+		// Delete Summary
+		if summary_data.SummaryDeleteRequest != nil {
+			
+			// TODO: check auth token
+
+			_, err = db.Exec(`DELETE FROM Summaries WHERE id = ?`, summary_data.SummaryDeleteRequest.SummaryID)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		// Unlike Summary
+		} else if summary_data.SummaryLikeDeleteRequest != nil {
+			
+			// TODO: check auth token
+
+			_, err = db.Exec(`DELETE FROM 'Summary Likes' WHERE id = ?`, summary_data.SummaryLikeDeleteRequest.SummaryLikeID)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, map[string]string{"status": "accepted"})
+
 	})
 
 	// LINKS
@@ -448,10 +485,12 @@ type EditPfpRequest struct {
 type SummaryRequest struct {
 	*SummaryCreateRequest
 	*SummaryLikeRequest
+	*SummaryDeleteRequest
+	*SummaryLikeDeleteRequest
 }
 
 func (a *SummaryRequest) Bind(r *http.Request) error {
-	if a.SummaryCreateRequest == nil && a.SummaryLikeRequest == nil {
+	if a.SummaryCreateRequest == nil && a.SummaryLikeRequest == nil && a.SummaryDeleteRequest == nil && a.SummaryLikeDeleteRequest == nil {
 		return errors.New("missing required Summary fields")
 	}
 
@@ -464,9 +503,19 @@ type SummaryCreateRequest struct {
 	Text string `json:"text"`
 }
 
+type SummaryDeleteRequest struct {
+	// would use json:"summary_id" here but conflicts with
+	// below SummaryLikeRequest json ... not sure how else to fix
+	SummaryID string `json:"summary_id_del"`
+}
+
 type SummaryLikeRequest struct {
-	SummaryID string `json:"summary_id,omitempty"`
-	UserID string `json:"user_id,omitempty"`
+	SummaryID string `json:"summary_id"`
+	UserID string `json:"user_id"`
+}
+
+type SummaryLikeDeleteRequest struct {
+	SummaryLikeID string `json:"slike_id"`
 }
 
 // LINK
