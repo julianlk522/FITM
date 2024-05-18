42eb8975f426a404fc0f19d918f157ad3a4016ff
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2024-05-18 15:07:44 -0400

    option to upload new profile pic in ProfilePic.tsx

--- frontend/src/components/ProfilePic.tsx
+++ frontend/src/components/ProfilePic.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'preact/hooks'
 
 interface Props {
-	pfp: string
     login_name: string
+	pfp: string | undefined
+    signed_in_user: string | undefined
+    token: string | undefined
 }
 
 export default function ProfilePic(props: Props) {
-    const {login_name, pfp} = props
+    const {login_name, pfp, signed_in_user, token} = props
     const [url, set_url] = useState<string | undefined>(undefined)
+
+    const is_signed_in_user = login_name === signed_in_user
 
     useEffect(() => {
         async function get_pfp() {
-            
+            if (!pfp) {
+                return
+            }
             const pfp_resp = await fetch(`http://127.0.0.1:8000/pic/${pfp}`, {
                 headers: { 'Content-Type': 'image/png' },
             })
             if (pfp_resp.status > 399) {
-                console.log(pfp_resp)
+                console.error(pfp_resp)
+                return
             }
             const pfp_blob = await pfp_resp.blob()
             const pfp_url = URL.createObjectURL(pfp_blob)
@@ -25,7 +32,52 @@ export default function ProfilePic(props: Props) {
 
         get_pfp()
     }, [pfp])
+
+    async function handle_pic_change(e: Event) {
+        if (!token) {
+            return (window.location.href = '/login')
+        }
+        const target = e.target as HTMLInputElement
+        if (!target.files) {
+            return
+        }
+
+        const new_pic = target.files[0]
+        let formData = new FormData()
+        formData.append('pic', new_pic)
+        
+        const new_pic_resp = await fetch(`http://127.0.0.1:8000/pic`, {
+            method: 'POST',
+            headers: { 'Authorization': `Bearer ${token}` },
+            body: formData
+        })
+        if (new_pic_resp.status > 399) {
+            console.error(new_pic_resp)
+            return
+        }
+
+        const new_pic_data = await new_pic_resp.blob()
+        set_url(URL.createObjectURL(new_pic_data))
+    }
     
 
-    return <img src={url} alt={login_name} width="150" />
+    return url
+        // user has profile pic
+        ? 
+            is_signed_in_user 
+                ?
+                    <>
+                        <img src={url} alt="profile pic" width='150' />
+                        <input type="file" accept={"image/*"} onChange={handle_pic_change} >Upload new</input>
+                    </>
+                :
+                    <img src={url} alt="profile pic" width='150' />
+        // no profile pic
+        : 
+            is_signed_in_user
+                ? 
+                    <>
+                        <input type="file" accept={"image/*"} onChange={handle_pic_change}>Upload profile pic</input>
+                    </>
+                : null
 }
