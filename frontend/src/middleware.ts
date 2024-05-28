dfb6629b7409ee4229511a51bd71354a629ded59
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2024-05-28 15:22:03 -0400

    declare global frontend namespace for context.locals in .env.d.ts, remove @ts-ignore where possible in index.astro / Layout.astro / middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,16 @@
 
 import { defineMiddleware } from "astro:middleware";
+import type { VerifyErrors } from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 import get_cookie from "./util/get_cookie";
 
 export const onRequest = defineMiddleware((context, next) => {
 
+    // when redirecting from login:
     // TODO: replace localhost URLs with production URLs
     if (context.request.url === 'http://localhost:4321/' && context.request.headers.get('Referer') === 'http://localhost:4321/login') {
+
+        // get JWT token cookie from request headers
         let cookie_string = context.request.headers.get('cookie')
         if (cookie_string) {
             const token = get_cookie('token', cookie_string)
@@ -14,15 +18,18 @@ export const onRequest = defineMiddleware((context, next) => {
                 return next();
             }
             
-            // check if jwt token is valid
-            jwt.verify(token, 'secret', function (err: any, decoded) {
-                // @ts-ignore
-                if (!err && decoded.login_name) {
+            // verify token
+            jwt.verify(token, 'secret', function<JwtPayload> (err: VerifyErrors | null, decoded: JwtPayload | undefined) {
+                if (decoded && !err) {
+
+                    // add token and user cookies if verified
                     // @ts-ignore
                     context.locals.user = decoded.login_name
                 }
             })
         }   
     }
+    // if not redirecting from login or no token or token expired/unverified, continue unauthenticated
     return next();
+    // (Layout.astro will run an additional JWT check and reset the cookie if expired/unverified. It cannot be performed in this middleware since not .astro)
 });
