3f2bb6aa50cbe904d824ec2e13ee4f35fd5516e8
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2024-08-02 14:50:34 -0400

    new link page looking pretty good

--- frontend/src/components/NewLinks.tsx
+++ frontend/src/components/NewLinks.tsx
@@ -1,113 +1,109 @@
-import { useState } from 'preact/hooks';
-import type { ErrorResponse, LinkData } from "../types";
-import { is_error_response } from "../types";
-import Link from './Link';
-import './NewLinks.css';
-import NewTag from './NewTag';
+import { useState } from 'preact/hooks'
+import type { ErrorResponse, LinkData } from '../types'
+import { is_error_response } from '../types'
+import Link from './Link'
+import './NewLinks.css'
+import NewTag from './NewTag'
 interface Props {
 	Token: string
-    User: string
+	User: string
 }
 
 export default function NewLinks(props: Props) {
-    const {Token: token} = props
+	const { Token: token } = props
 
-    const [error, set_error] = useState<string | undefined>(undefined)
-    const [categories, set_categories] = useState<string[]>([])
-    const [submitted_links, set_submitted_links] = useState<LinkData[]>([])
+	const [error, set_error] = useState<string | undefined>(undefined)
+	const [categories, set_categories] = useState<string[]>([])
+	const [submitted_links, set_submitted_links] = useState<LinkData[]>([])
 
-    async function handle_submit(event: SubmitEvent) {
-        event.preventDefault()
-        const form = event.target as HTMLFormElement
-        const formData = new FormData(form)
-        const url = formData.get('url')
-        const summary = formData.get('summary')
+	async function handle_submit(event: SubmitEvent) {
+		event.preventDefault()
+		const form = event.target as HTMLFormElement
+		const formData = new FormData(form)
+		const url = formData.get('url')
+		const summary = formData.get('summary')
 
-        let resp_body: string
+		let resp_body: string
 
-        if (summary) {
-            resp_body = JSON.stringify({
-                url,
-                categories: categories.join(','),
-                summary
-            })
-        } else {
-            resp_body = JSON.stringify({
-                url,
-                categories: categories.join(','),
-            })
-        }
-    
-        const new_link_resp = await fetch('http://127.0.0.1:8000/links', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-            body: resp_body,
-        })
-        if (new_link_resp.statusText === "Unauthorized") {
-            window.location.href = '/login'
-        }
-        let new_link_data: LinkData | ErrorResponse = await new_link_resp.json()
+		if (summary) {
+			resp_body = JSON.stringify({
+				url,
+				categories: categories.join(','),
+				summary,
+			})
+		} else {
+			resp_body = JSON.stringify({
+				url,
+				categories: categories.join(','),
+			})
+		}
 
-        if (is_error_response(new_link_data)) {
-            set_error(new_link_data.error)
-            return
-        } else {
-            new_link_data.IsTagged = true
-            set_submitted_links([...submitted_links, new_link_data])
-            set_categories([])
-            set_error(undefined)
-            form.reset()
-        }
-    
-        return
-    }
+		const new_link_resp = await fetch('http://127.0.0.1:8000/links', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: resp_body,
+		})
+		if (new_link_resp.statusText === 'Unauthorized') {
+			window.location.href = '/login'
+		}
+		let new_link_data: LinkData | ErrorResponse = await new_link_resp.json()
 
-    return (
-        <section>
-            <h2>Link Details</h2>
-            
-            {error
-                ? 
-                    <p class='error'>{`Error: ${error}`}</p>
-                :
-                    null
-            }
+		if (is_error_response(new_link_data)) {
+			set_error(new_link_data.error)
+			return
+		} else {
+			new_link_data.IsTagged = true
+			set_submitted_links([...submitted_links, new_link_data])
+			set_categories([])
+			set_error(undefined)
+			form.reset()
+		}
 
-            <form onSubmit={async (e) => await handle_submit(e)}>
-                <label for='url'>URL</label>
-                <input type='text' id='url' name='url' />
-                <br />
+		return
+	}
 
-                <label for='summary'>Summary (optional)</label>
-                <textarea id='summary' name='summary' rows={3} cols={50} />
-                <br />
+	return (
+		<section id='new-link'>
+			<h2>New Link</h2>
 
-                <NewTag Categories={categories} SetCategories={set_categories} SetError={set_error} />
-                
-                <input type='submit' value='Submit' />
-            </form>
+			{error ? <p class='error'>{`Error: ${error}`}</p> : null}
 
-            {submitted_links.length ? (
-                <div id='submitted'>
-                    <h2>Submitted Links</h2>
-                    <ol>
-                        {submitted_links.map((link) => (
-                            <Link
-                                key={link.ID}
-                                Link={link}
-                                Token={props.Token}
-                                User={props.User}
-                                IsSummaryPage={false}
-                                IsTagPage={false} 
-                            />
-                        ))}
-                    </ol>
-                </div>
-            ) : null}
-        </section>
-    )
-    
+			<form onSubmit={async (e) => await handle_submit(e)}>
+				<label for='url'>URL</label>
+				<input type='text' id='url' name='url' />
+
+				<label for='summary'>Summary (optional)</label>
+				<textarea id='summary' name='summary' rows={3} cols={50} />
+
+				<NewTag
+					Categories={categories}
+					SetCategories={set_categories}
+					SetError={set_error}
+				/>
+
+				<input type='submit' value='Submit' />
+			</form>
+
+			{submitted_links.length ? (
+				<div id='submitted'>
+					<h2>Submitted Links</h2>
+					<ol>
+						{submitted_links.map((link) => (
+							<Link
+								key={link.ID}
+								Link={link}
+								Token={props.Token}
+								User={props.User}
+								IsSummaryPage={false}
+								IsTagPage={false}
+							/>
+						))}
+					</ol>
+				</div>
+			) : null}
+		</section>
+	)
 }
--- frontend/src/components/NewTag.tsx
+++ frontend/src/components/NewTag.tsx
@@ -1,72 +1,79 @@
-import { effect, useSignal } from "@preact/signals";
-import type { Dispatch, StateUpdater } from "preact/hooks";
-import './NewTag.css';
-import TagCategory from './TagCategory';
+import { effect, useSignal } from '@preact/signals'
+import type { Dispatch, StateUpdater } from 'preact/hooks'
+import './NewTag.css'
+import TagCategory from './TagCategory'
 
 interface Props {
-    Categories: string[]
-    SetCategories: Dispatch<StateUpdater<string[]>>
-    SetError: Dispatch<StateUpdater<string | undefined>>
+	Categories: string[]
+	SetCategories: Dispatch<StateUpdater<string[]>>
+	SetError: Dispatch<StateUpdater<string | undefined>>
 }
 
 export default function NewTag(props: Props) {
-    const { Categories: categories, SetCategories: set_categories, SetError: set_error } = props
+	const {
+		Categories: categories,
+		SetCategories: set_categories,
+		SetError: set_error,
+	} = props
 
-    // Pass deleted_cat signal to children TagCategory.tsx
-    // to allow removing their category in this NewTag.tsx parent
-    const deleted_cat = useSignal<string | undefined>(undefined)
-    
-    // Check for deleted category and set categories accordingly
-    effect(() => {
-        if (deleted_cat.value) {
-            set_categories((c) => c.filter(cat => cat !== deleted_cat.value))
-            deleted_cat.value = undefined
-        }
-    })
-    
-    function add_category(event: MouseEvent) {
-        event.preventDefault()
+	// Pass deleted_cat signal to children TagCategory.tsx
+	// to allow removing their category in this NewTag.tsx parent
+	const deleted_cat = useSignal<string | undefined>(undefined)
 
-        // @ts-ignore
-        const form = event.target.form as HTMLFormElement
-        if (!form) return set_error('Form not found')
-        const formData = new FormData(form)
-        const category = formData.get('category')?.toString()
+	// Check for deleted category and set categories accordingly
+	effect(() => {
+		if (deleted_cat.value) {
+			set_categories((c) => c.filter((cat) => cat !== deleted_cat.value))
+			deleted_cat.value = undefined
+		}
+	})
 
-        if (!category) {
-            set_error('Missing category')
-            return
-        }
+	function add_category(event: MouseEvent) {
+		event.preventDefault()
 
-        if (categories.includes(category)) {
-            set_error('Category already added')
-            return
-        }
+		// @ts-ignore
+		const form = event.target.form as HTMLFormElement
+		if (!form) return set_error('Form not found')
+		const formData = new FormData(form)
+		const category = formData.get('category')?.toString()
 
-        set_categories([...categories, category].sort())
-        set_error(undefined)
+		if (!category) {
+			set_error('Missing category')
+			return
+		}
 
-        const cat_field = document.getElementById("category") as HTMLInputElement
-        cat_field.value = ""
-        return
-    }
+		if (categories.includes(category)) {
+			set_error('Category already added')
+			return
+		}
 
-    return (
-        <>
-            <label for='category'>Tag Category(ies)</label>
-            <input type='text' id='category' name='category' />
-            <button onClick={(event) => add_category(event)}>Add Category</button>
+		set_categories([...categories, category].sort())
+		set_error(undefined)
 
-            <ol id='categories_grid'>
-                {categories.map((cat) => (
-                    <TagCategory
-                    Category={cat}
-                    EditActivated={true}
-                    Deleted={deleted_cat}
-                />
-                ))}
-            </ol>
-        </>
-    )
-    
+		const cat_field = document.getElementById(
+			'category'
+		) as HTMLInputElement
+		cat_field.value = ''
+		return
+	}
+
+	return (
+		<>
+			<label for='category'>Tag Cat(s)</label>
+			<input type='text' id='category' name='category' />
+			<button id='add-category' onClick={(event) => add_category(event)}>
+				Add
+			</button>
+
+			<ol id='categories_grid'>
+				{categories.map((cat) => (
+					<TagCategory
+						Category={cat}
+						EditActivated={true}
+						Deleted={deleted_cat}
+					/>
+				))}
+			</ol>
+		</>
+	)
 }
