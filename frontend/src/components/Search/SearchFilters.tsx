e1653cb596b178898025a3c42d2107d7cf94f133
Julian Lindsay-Kaufman <101271303+julianlk522@users.noreply.github.com>
2024-09-27 20:32:56 -0400

    add SearchNSFW.tsx, add SearchNSFW to SearchFilters.tsx, get nsfw URL params in top page and edit get links URL accordingly

--- frontend/src/components/Search/SearchFilters.tsx
+++ frontend/src/components/Search/SearchFilters.tsx
@@ -3,19 +3,22 @@ import { useState } from 'preact/hooks'
 import type { Period, SortMetric } from '../../types'
 import SearchCats from './SearchCats'
 import './SearchFilters.css'
+import SearchNSFW from './SearchNSFW'
 import SearchPeriod from './SearchPeriod'
 import SearchSortBy from './SearchSortBy'
 
 interface Props {
 	InitialPeriod: Period
 	InitialSortBy: SortMetric
-	InitialCats: string[] | undefined
+	InitialCats: string[]
+	InitialNSFW: boolean
 }
 
 export default function SearchFilters(props: Props) {
 	const [period, set_period] = useState<Period>(props.InitialPeriod)
 	const [sort_by, set_sort_by] = useState<SortMetric>(props.InitialSortBy)
-	const [cats, set_cats] = useState<string[]>(props.InitialCats ?? [])
+	const [cats, set_cats] = useState<string[]>(props.InitialCats)
+	const [nsfw, set_nsfw] = useState<boolean>(props.InitialNSFW)
 
 	// set search URL based on period and cats
 	const base_URL = `/top`
@@ -24,7 +27,6 @@ export default function SearchFilters(props: Props) {
 	if (cats.length) {
 		search_URL += `?cats=${cats.join(',')}`
 	}
-
 	if (period !== 'all') {
 		if (cats.length) {
 			search_URL += `&period=${period}`
@@ -32,7 +34,6 @@ export default function SearchFilters(props: Props) {
 			search_URL += `?period=${period}`
 		}
 	}
-
 	if (sort_by !== 'rating') {
 		if (cats.length || period !== 'all') {
 			search_URL += `&sort_by=${sort_by}`
@@ -40,14 +41,17 @@ export default function SearchFilters(props: Props) {
 			search_URL += `?sort_by=${sort_by}`
 		}
 	}
-
-	// pass changed_period to SearchPeriod.tsx to allow modifying period state in SearchFilters.tsx
-	const changed_period = useSignal<Period>(props.InitialPeriod)
-
-	// pass changed_sort_by to SearchSortBy.tsx to allow modifying sort_by state in SearchFilters.tsx
-	const changed_sort_by = useSignal<SortMetric>(props.InitialSortBy)
+	if (nsfw) {
+		if (cats.length || period !== 'all' || sort_by !== 'rating') {
+			search_URL += `&nsfw=true`
+		} else {
+			search_URL += `?nsfw=true`
+		}
+	}
 
 	// Check for update period / sort_by, set state accordingly
+	const changed_period = useSignal<Period>(props.InitialPeriod)
+	const changed_sort_by = useSignal<SortMetric>(props.InitialSortBy)
 	effect(() => {
 		if (changed_period.value) {
 			set_period(changed_period.value)
@@ -72,10 +76,9 @@ export default function SearchFilters(props: Props) {
 					SetSortBySignal={changed_sort_by}
 				/>
 
-				<SearchCats
-					SelectedCats={cats}
-					SetSelectedCats={set_cats}
-				/>
+				<SearchCats SelectedCats={cats} SetSelectedCats={set_cats} />
+
+				<SearchNSFW NSFW={nsfw} SetNSFW={set_nsfw} />
 
 				<a id='search-from-filters' href={search_URL}>
 					Search
